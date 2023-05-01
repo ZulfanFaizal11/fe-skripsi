64b5d2e0f9e5114ccfac8e2ade28f4677f57bc58
RizkiB12 <rbagus306@gmail.com>
2023-05-02 03:58:05 +0700

    update: add new structure for admin and landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import 'antd/dist/reset.css';
-import './index.css'
-import LayoutAdmin from './components/LayoutAdmin'
-import About from './pages/About'
-import NotFoundPage from './pages/404'
-import LandingPage from './pages/LandingPage';
-import HomeAdmin from './pages/HomeAdmin';
-import SettingAdmin from './pages/SettingAdmin';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Booking from './pages/Booking';
-import TutorialBooking from './pages/TutorialBooking';
-import Contact from './pages/Contact';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "antd/dist/reset.css";
+import "./index.css";
+import LayoutAdmin from "./components/LayoutAdmin";
+import About from "./pages/About";
+import NotFoundPage from "./pages/404";
+import LandingPage from "./pages/LandingPage";
+import HomeAdmin from "./pages/HomeAdmin";
+import SettingAdmin from "./pages/SettingAdmin";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Booking from "./pages/Booking";
+import TutorialBooking from "./pages/TutorialBooking";
+import Contact from "./pages/Contact";
 
 const router = createBrowserRouter([
   {
@@ -31,23 +28,23 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "register",
-    element: <Register />
+    element: <Register />,
   },
   {
     path: "booking",
-    element: <Booking />
+    element: <Booking />,
   },
   {
     path: "tutorial-booking",
-    element: <TutorialBooking />
+    element: <TutorialBooking />,
   },
   {
     path: "contact",
-    element: <Contact />
+    element: <Contact />,
   },
   {
     path: "/admin",
@@ -62,12 +59,12 @@ const router = createBrowserRouter([
         path: "settings",
         element: <SettingAdmin />,
       },
-    ]
-  }
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
