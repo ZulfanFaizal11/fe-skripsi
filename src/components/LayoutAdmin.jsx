987b3fc605808443e7b59a407505b26cef83ca4f
RizkiB12 <rbagus306@gmail.com>
2023-05-02 03:15:30 +0700

    update: remake a sentence laooyut to layout

--- src/components/LayoutAdmin.jsx
+++ src/components/LayoutAdmin.jsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
-const LaooutAdmin = () => {
-    return (
-        <>
-            <div style={{ backgroundColor: 'red' }}>This wrapped with layout admin ges</div>
-            <Outlet />
-        </>
-    )
-}
+const LayoutAdmin = () => {
+  return (
+    <>
+      <div style={{ backgroundColor: "red" }}>This wrapped with layout admin ges</div>
+      <Outlet />
+    </>
+  );
+};
 
-export default LaooutAdmin
+export default LayoutAdmin;
