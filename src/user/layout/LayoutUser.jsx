f4c62fc9341a43b96c1af1ad2acb47e582e78cd1
RizkiB12 <rbagus306@gmail.com>
2023-05-16 00:00:02 +0700

    update: remake a icon and give a background

--- src/user/layout/LayoutUser.jsx
+++ src/user/layout/LayoutUser.jsx
@@ -1,6 +1,6 @@
 import { Layout, Row, Col, Typography, Button } from "antd";
 import { useNavigate } from "react-router-dom";
-import { FacebookFilled, TwitterSquareFilled, InstagramFilled } from "@ant-design/icons";
+import { FacebookFilled, TwitterSquareFilled, InstagramFilled, PhoneOutlined, WhatsAppOutlined } from "@ant-design/icons";
 import styled from "styled-components";
 const { Header, Footer } = Layout;
 const { Title } = Typography;
@@ -80,9 +80,9 @@ function LayoutUser() {
           </Col>
         </WrapNavbar>
       </Header>
-      <hr />
+      <hr style={{ margin: "0" }} />
 
-      <Row justify="center" style={{ padding: "50px 0 50px 0", backgroundColor: "#ffffff", color: "#005F6C" }}>
+      <Row justify="center" style={{ padding: "50px 0 50px 0", backgroundColor: "#F7F9FA", color: "#005F6C" }}>
         <Col xs={24}>
           <Row justify="center">
             <Col xs={20} lg={18}>
@@ -91,7 +91,9 @@ function LayoutUser() {
                   <Title style={{ color: "#923421", fontWeight: "bold", textDecoration: "underline" }} level={5}>
                     GEDUNG MAHKOTA WULUNG
                   </Title>
-                  <p>Jl. Arumba, Tunggulwulung, Kec.Lowokwaru, Kota Malang, Jawa Timur 65143</p>
+                  <p>
+                    Jl. Arumba, Tunggulwulung, Kec.Lowokwaru, <br /> Kota Malang, Jawa Timur 65143
+                  </p>
                 </Col>
                 <Col style={{ margin: "10px 0 10px 0" }} xs={24} lg={6}>
                   <Title style={{ color: "#005F6C" }} level={5}>
@@ -106,21 +108,33 @@ function LayoutUser() {
                   </Title>
                   <p>Weekdays & Weekend</p>
                   <p>(09.00 - 20.00)</p>
-                  <p>06182002200</p>
-                  <p>082267138775</p>
+                  <p>
+                    <i style={{ fontSize: "20px", color: "#FFFFFF", backgroundColor: "#005F6C", margin: "15px 10px 10px 0", padding: "2px 5px", borderRadius: "5px" }}>
+                      <PhoneOutlined />
+                    </i>
+                    06182002200
+                  </p>
+                  <p>
+                    <i>
+                      <i style={{ fontSize: "20px", color: "#FFFFFF", backgroundColor: "#005F6C", margin: "15px 10px 10px 0", padding: "2px 5px", borderRadius: "5px" }}>
+                        <WhatsAppOutlined />
+                      </i>
+                    </i>
+                    082267138775
+                  </p>
                 </Col>
                 <Col style={{ margin: "10px 0 10px 0" }} xs={24} lg={6}>
                   <Title style={{ color: "#005F6C" }} level={5}>
                     Follow Kami
                   </Title>
                   <Row>
-                    <p style={{ fontSize: "25px", marginRight: "5px" }}>
+                    <p style={{ fontSize: "2rem", marginRight: "10px" }}>
                       <FacebookFilled />
                     </p>
-                    <p style={{ fontSize: "25px", marginRight: "5px" }}>
+                    <p style={{ fontSize: "2rem", marginRight: "10px" }}>
                       <InstagramFilled />
                     </p>
-                    <p style={{ fontSize: "25px", marginRight: "5px" }}>
+                    <p style={{ fontSize: "2rem" }}>
                       <TwitterSquareFilled />
                     </p>
                   </Row>
